86b6ded7ed587245f3c2013c1af0e93accbcc09e
lrh733 <2534117254@qq.com>
2020-01-23 20:01:08 +0800

    生成数据版本2

--- lrh/02.py
+++ lrh/02.py
@@ -1,9 +1,11 @@
 import torch
-
+import matplotlib.pyplot as plt
 
 #生成数据
 feature = torch.rand(1000, 2)
 true_w = torch.tensor([1, 2],dtype=torch.float).view(-1,1)
 true_b = 4.1
-labels = torch.mm(feature, true_w) + b + torch.normal(0,0.01,size=(1000,1))
+labels = torch.mm(feature, true_w) + true_b + torch.normal(0,0.01,size=(1000,1))
 
+
+print(labels)
