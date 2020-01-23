666816d53a01965424d6c42547420e44bb8d04c6
lrh733 <2534117254@qq.com>
2020-01-23 19:52:11 +0800

    生成数据

---
+++ lrh/02.py
@@ -0,0 +1,9 @@
+import torch
+
+
+#生成数据
+feature = torch.rand(1000, 2)
+true_w = torch.tensor([1, 2],dtype=torch.float).view(-1,1)
+true_b = 4.1
+labels = torch.mm(feature, true_w) + b + torch.normal(0,0.01,size=(1000,1))
+
